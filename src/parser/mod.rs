12d9ba90633fb11aaf44da0f743132e87c6d140f
Simon CROS <simon.dc.cros@gmail.com>
2023-04-03 23:45:29 +0200

    Added negation

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,46 +10,157 @@ pub struct Parser {
     tokens: Peekable<IntoIter<Token>>,
 }
 
-impl Parser {
+impl<'a> Parser {
     pub fn new(tokens: Vec<Token>) -> Self {
         Self {
             tokens: tokens.into_iter().peekable(),
         }
     }
 
-    pub fn parse(mut self) -> Result<Node<'static>, String> {
-        let mut tokens: Vec<Token> = Vec::<Token>::new();
-        
-		let root = self.expr();
+    pub fn parse(mut self) -> Result<Box<Node>, String> {
+		let root = self.assignation();
+
 		if self.tokens.peek().is_some() {
 			return Err(format!("Unexpected token : {:?}", self.tokens.peek().unwrap()));
 		}
-
-		Ok(root)
+		if root.is_none() {
+			return Err(format!("Expected token at end"));
+		}
+		Ok(root.unwrap())
     }
 
-	fn expr(&self) -> Node<'static> {
-		let result = self.term();
+	fn assignation(&mut self) -> Option<Box<Node>> {
+		let lhs = self.expr();
+		if lhs.is_none() {
+			return None;
+		}
 
-		loop {
-			let token = self.tokens.peek();
+		let token = self.tokens.peek();
+		match token {
+			Some(Token::Equal(_)) => {
+				self.tokens.next();
+				match self.expr() {
+					Some(rhs) => return Some(Box::new(Node::Equal(lhs.unwrap(), rhs))),
+					None => return None,
+				}
+			},
+			_ => return lhs,
+		}
+	}
+
+	fn expr(&mut self) -> Option<Box<Node>> {
+		let lhs = self.factor();
+		if lhs.is_none() {
+			return None;
+		}
+				
+		let mut result: Box<Node> = lhs.unwrap();
+		while let Some(token) = self.tokens.peek() {
 			match token {
-				Some(Token::Add(x)) => result = Node::Add(&result, &self.term()),
-				Some(Token::Sub(x)) => result = Node::Sub(&result, &self.term()),
+				Token::Add(_) => {
+					self.tokens.next();
+					match self.factor() {
+						Some(rhs) => result = Box::new(Node::Add(result, rhs)),
+						None => return None,
+					}
+				},
+				Token::Sub(_) => {
+					self.tokens.next();
+					match self.factor() {
+						Some(rhs) => result = Box::new(Node::Sub(result, rhs)),
+						None => return None,
+					}
+				},
 				_ => break,
 			}
-			self.tokens.next();
+		}
+		return Some(result);
+	}
+
+	fn factor(&mut self) -> Option<Box<Node>> {
+		let lhs = self.pow();
+		if lhs.is_none() {
+			return None;
+		}
+
+		let mut result: Box<Node> = lhs.unwrap();
+		while let Some(token) = self.tokens.peek() {
+			match token {
+				Token::Mul(_) => {
+					self.tokens.next();
+					match self.pow() {
+						Some(rhs) => result = Box::new(Node::Mul(result, rhs)),
+						None => return None,
+					}
+				},
+				Token::Div(_) => {
+					self.tokens.next();
+					match self.pow() {
+						Some(rhs) => result = Box::new(Node::Div(result, rhs)),
+						None => return None,
+					}
+				},
+				_ => break,
+			}
+		}
+		return Some(result);
+	}
+
+	fn pow(&mut self) -> Option<Box<Node>> {
+		let lhs = self.term();
+		if lhs.is_none() {
+			return None;
+		}
+
+		let mut result: Box<Node> = lhs.unwrap();
+		while let Some(token) = self.tokens.peek() {
+			match token {
+				Token::Pow(_) => {
+					self.tokens.next();
+					match self.term() {
+						Some(rhs) => result = Box::new(Node::Pow(result, rhs)),
+						None => return None,
+					}
+				},
+				_ => break,
+			}
+		}
+		return Some(result);
+	}
+
+	fn term<'b>(&mut self) -> Option<Box<Node>> {
+		let mut result: Option<Box<Node>>;
+
+		let token = self.tokens.peek();
+		match token {
+			Some(Token::Sub(_)) => 
+			{
+				self.tokens.next();
+				result = self.term();
+				match result {
+					Some(next) => result = Some(Box::new(Node::Negate(next))),
+					_ => return None,
+				}
+			},
+			Some(Token::Number(n)) => {
+				result = Some(Box::new(Node::Number(*n)));
+				self.tokens.next();
+			},
+			Some(Token::Identifier(n)) => {
+				result = Some(Box::new(Node::Identifier(n.clone())));
+				self.tokens.next();
+			},
+			Some(Token::LParent(_)) => {
+				self.tokens.next();
+				result = self.expr();
+				if !matches!(self.tokens.peek(), Some(Token::RParent(_)))
+				{
+					return None;
+				}
+				self.tokens.next();
+			},
+			_ => return None,
 		}
 		result
-	}
-
-	fn term(&self) -> Node<'static> {
-		let result = self.factor();
-		
-		todo!()
-	}
-
-	fn factor(&self) -> Node<'static> {
-		todo!()
 	}
 }
--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,25 +1,27 @@
 use std::fmt::Debug;
 
-pub enum Node<'a> {
-    Add(&'a Node<'a>, &'a Node<'a>),
-    Sub(&'a Node<'a>, &'a Node<'a>),
-    Div(&'a Node<'a>, &'a Node<'a>),
-    Mul(&'a Node<'a>, &'a Node<'a>),
-    Pow(&'a Node<'a>, &'a Node<'a>),
-    Equal(&'a Node<'a>, &'a Node<'a>),
+pub enum Node {
+    Add(Box<Node>, Box<Node>),
+    Sub(Box<Node>, Box<Node>),
+    Div(Box<Node>, Box<Node>),
+    Mul(Box<Node>, Box<Node>),
+    Pow(Box<Node>, Box<Node>),
+    Equal(Box<Node>, Box<Node>),
+    Negate(Box<Node>),
     Identifier(String),
     Number(f32),
 }
 
-impl Debug for Node<'_> {
+impl Debug for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Add(l, r) => write!(f, "({:?} = {:?})", l, r),
-            Self::Sub(l, r) => write!(f, "({:?} = {:?})", l, r),
-            Self::Mul(l, r) => write!(f, "({:?} = {:?})", l, r),
-            Self::Div(l, r) => write!(f, "({:?} = {:?})", l, r),
-            Self::Pow(l, r) => write!(f, "({:?} = {:?})", l, r),
+            Self::Add(l, r) => write!(f, "({:?} + {:?})", l, r),
+            Self::Sub(l, r) => write!(f, "({:?} - {:?})", l, r),
+            Self::Mul(l, r) => write!(f, "({:?} * {:?})", l, r),
+            Self::Div(l, r) => write!(f, "({:?} / {:?})", l, r),
+            Self::Pow(l, r) => write!(f, "({:?} ^ {:?})", l, r),
             Self::Equal(l, r) => write!(f, "({:?} = {:?})", l, r),
+            Self::Negate(v) => write!(f, "-({:?})", v),
             Self::Identifier(v) => write!(f, "{:?}", v),
             Self::Number(v) => write!(f, "{:?}", v),
         }
