a1e032aedc4794d01506e3e5377297f9beb7fb7e
SimonCROS <scros@student.42lyon.fr>
2023-04-18 16:22:05 +0200

    Sort polynominal

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     env::{self, Args},
+    mem::swap,
     process::exit,
 };
 
 use lexer::Lexer;
-use parser::{Parser, node::Node};
+use parser::{node::Node, Parser};
 
 mod lexer;
 mod parser;
@@ -85,7 +86,7 @@ fn reduce(node: &mut Box<Node>) {
                 **node = Node::Number(-(*v));
             }
         }
-        _ => ()
+        _ => (),
     }
 }
 
@@ -96,18 +97,63 @@ fn remove_substractions(node: &mut Box<Node>) {
             remove_substractions(r);
             **node = Node::Add(l.clone(), Box::new(Node::Negate(r.clone())));
         }
-        Node::Add(l, r) => { remove_substractions(l); remove_substractions(r) }
-        Node::Mul(l, r) => { remove_substractions(l); remove_substractions(r) }
-        Node::Div(l, r) => { remove_substractions(l); remove_substractions(r) }
-        Node::Pow(l, r) => { remove_substractions(l); remove_substractions(r) }
-        Node::Equal(l, r) => { remove_substractions(l); remove_substractions(r) }
+        Node::Add(l, r) => {
+            remove_substractions(l);
+            remove_substractions(r);
+        }
+        Node::Mul(l, r) => {
+            remove_substractions(l);
+            remove_substractions(r);
+        }
+        Node::Div(l, r) => {
+            remove_substractions(l);
+            remove_substractions(r);
+        }
+        Node::Pow(l, r) => {
+            remove_substractions(l);
+            remove_substractions(r);
+        }
+        Node::Equal(l, r) => {
+            remove_substractions(l);
+            remove_substractions(r);
+        }
         Node::Negate(v) => remove_substractions(v),
-        _ => ()
+        _ => (),
     }
 }
 
-fn sort_polynominal(node: &mut Box<Node>) {
-    
+fn sort_polynominal(node: &mut Box<Node>, count: bool) -> f32 {
+    match node.as_mut() {
+        Node::Add(l, r) => {
+            let ldeg = sort_polynominal(l, count);
+            let rdeg = sort_polynominal(r, count);
+            if rdeg > ldeg {
+                std::mem::swap(l, r);
+                return rdeg;
+            }
+            ldeg
+        }
+        Node::Number(v) => {
+            if count {
+                *v
+            } else {
+                0f32
+            }
+        }
+        Node::Identifier(_) => 1f32,
+        Node::Pow(l, r) => {
+            let ldeg = sort_polynominal(l, count);
+            let rdeg = sort_polynominal(r, true);
+            if rdeg > ldeg {
+                return rdeg;
+            }
+            ldeg
+        }
+        Node::Mul(l, r) => sort_polynominal(l, count).max(sort_polynominal(r, count)),
+        Node::Sub(l, r) => sort_polynominal(l, count).max(sort_polynominal(r, count)),
+        Node::Negate(v) => sort_polynominal(v, count),
+        _ => 0f32,
+    }
 }
 
 fn main() {
@@ -124,24 +170,36 @@ fn main() {
             let parser = Parser::new(tokens);
             match parser.parse() {
                 Ok((mut lhs, mut rhs)) => {
-                    println!("Full\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m", lhs, rhs);
+                    println!(
+                        "Full\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m",
+                        lhs, rhs
+                    );
                     remove_substractions(&mut lhs);
-                    println!("No sub\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m", lhs, rhs);
+                    println!(
+                        "No sub\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m",
+                        lhs, rhs
+                    );
                     reduce(&mut lhs);
                     reduce(&mut rhs);
-                    println!("Reduced\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m", lhs, rhs);
-                    sort_polynominal(&mut lhs);
-                    println!("Sorted\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m", lhs, rhs);
-                },
+                    println!(
+                        "Reduced\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m",
+                        lhs, rhs
+                    );
+                    sort_polynominal(&mut lhs, false);
+                    println!(
+                        "Sorted\t- \x1b[33;1m{}\x1b[0m \x1b[31;1m=\x1b[0m \x1b[32;1m{}\x1b[0m",
+                        lhs, rhs
+                    );
+                }
                 Err(v) => {
                     eprintln!("{}", v);
                     exit(1);
                 }
             }
-        },
+        }
         Err(v) => {
             eprintln!("{}", v);
             exit(1);
-        },
+        }
     }
 }
