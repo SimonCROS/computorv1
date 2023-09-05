0a6e7f876ce886cfb190a26a84ce81ae9c32ef08
Simon CROS <simon.dc.cros@gmail.com>
2023-09-05 08:28:06 +0200

    Change last monominal test

--- Computorv1Tests/EquationToMonominalsTests.cs
+++ Computorv1Tests/EquationToMonominalsTests.cs
@@ -68,12 +68,12 @@ public class EquationToMonominalsTests
         Assert.IsTrue(new Lexer("3 * x^5 - 7 * x^4 + 2 * x^3 + 6 * x^2 - 5 * x + 9 = 4 * x^5 - 2 * x^4 + 8 * x^3 - 6 * x^2 + 7 * x - 12").Tokenize(out List<Token> tokens));
         Assert.IsTrue(new Parser(tokens).Parse(out Node? node));
         CollectionAssert.AreEqual(
-            new List<string>()
-                { "(3x ^ 5)", "(-7x ^ 4)", "(2x ^ 3)", "(6x ^ 2)", "-5x", "9" },
-            Utils.ListMonominals(((EqualNode)node).Left).ConvertAll(c => c.ToString()));
+            new List<Monominal>()
+                { new(3, "x", 5), new(-7, "x", 4), new(2, "x", 3), new(6, "x", 2), new(-5, "x"), new(9) },
+            Utils.ListMonominals(((EqualNode)node).Left));
         CollectionAssert.AreEqual(
-            new List<string>()
-                { "(4x ^ 5)", "(-2x ^ 4)", "(8x ^ 3)", "(-6x ^ 2)", "7x", "-12" },
-            Utils.ListMonominals(((EqualNode)node).Right).ConvertAll(c => c.ToString()));
+            new List<Monominal>()
+                { new(4, "x", 5), new(-2, "x", 4), new(8, "x", 3), new(-6, "x", 2), new(7, "x"), new(-12) },
+            Utils.ListMonominals(((EqualNode)node).Right));
     }
 }
